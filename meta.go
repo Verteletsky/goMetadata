fb88933a1c9febbd072350812573860f6692bb59
verteletsky <verteletsky@bitrix24.ru>
2022-09-30 16:57:15 +0300

    fix check nil value

--- meta.go
+++ meta.go
@@ -9,7 +9,9 @@ import (
 )
 
 const (
-	ADMIN = "admin"
+	ADMIN     = "admin"
+	ActorId   = "x-actor-id"
+	ActorType = "x-actor-type"
 )
 
 func CheckMetaData(ctx context.Context) (string, string, error) {
@@ -17,14 +19,17 @@ func CheckMetaData(ctx context.Context) (string, string, error) {
 	userType := ""
 	md, ok := metadata.FromIncomingContext(ctx)
 	if ok {
-		switch md.Get("x-actor-type")[0] {
+		if md.Get(ActorType) == nil || md.Get(ActorId) == nil {
+			return "", "", errors.New("headers nil")
+		}
+		switch md.Get(ActorType)[0] {
 		case ADMIN:
 			userType = ADMIN
 		default:
 			return "", "", errors.New("access denied")
 		}
 
-		uID := md.Get("x-actor-id")[0]
+		uID := md.Get(ActorId)[0]
 		if len(uID) > 0 {
 			userID = uID
 		} else {
@@ -39,10 +44,10 @@ func CheckMetaData(ctx context.Context) (string, string, error) {
 
 func CustomMatcherHeader(key string) (string, bool) {
 	switch strings.ToLower(key) {
-	case "x-actor-id":
-		return "x-actor-id", true
-	case "x-actor-type":
-		return "x-actor-type", true
+	case ActorId:
+		return ActorId, true
+	case ActorType:
+		return ActorType, true
 	default:
 		return runtime.DefaultHeaderMatcher(key)
 	}
